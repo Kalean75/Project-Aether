524e5ef6f62dddf9df5e6a657eb154d0ba8c4592
Kalean75 <43306850+Kalean75@users.noreply.github.com>
2023-08-04 23:01:15 -0600

    Started on playercontroller

--- Assets/Src/PlayerController.cs
+++ Assets/Src/PlayerController.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("PlayerAttributes")]
+	[SerializeField] float movementSpeed = 2.0f;
+    [SerializeField] int[] playerSprites;
+	// Start is called before the first frame update
+	void Start()
     {
         
     }
@@ -13,6 +17,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        movePlayer();
     }
+
+    //z and x axis for horizontal
+    private void movePlayer()
+    {
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
+        var newPosz = transform.position.z + z;
+        var newPosX = transform.position.x + x;
+        transform.position = new Vector3(newPosX, 0, newPosz);
+	}
 }
